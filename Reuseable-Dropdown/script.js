8e97a00a651e16c3957ce9d98ddef4c00a61b561
Tanapol Yootaworn <85988039+TanapolQue@users.noreply.github.com>
2024-08-07 23:00:12 +0700

    Add files via upload

---
+++ Reuseable-Dropdown/script.js
@@ -0,0 +1,40 @@
+class Dropdown {
+
+    constructor() {
+        this.dropdowns = document.querySelectorAll('.dropdown .dropdown-menu');
+        if (this. dropdowns.length) {
+            this.initialize();
+        }
+    }
+
+    initialize() {
+        document.addEventListener('click', (e) => {
+            if (e.target.classList.contains('dropdown-action')) {
+                this.hideOtherDropdowns(e.target);
+                this.handleClick(e.target);
+            } else {
+                this.hideOtherDropdowns(null);
+            }
+        })
+    }
+
+    handleClick(dropdownAction) {
+        this. dropdowns.forEach(dropdown => {
+            if (dropdown.parentElement.contains(dropdownAction)) {
+                dropdown.classList.add('active');
+            } else {
+                dropdown.classList.remove('active')
+            }
+        })
+    }
+
+    hideOtherDropdowns(dropdownAction) {
+        this.dropdowns.forEach((dropdown) => {
+            if (!dropdown.parentElement.contains(dropdownAction))  {
+                dropdown.classList.remove('active');
+            }
+        });
+    }
+}
+
+document.addEventListener('DOMContentLoaded', () => new Dropdown);
